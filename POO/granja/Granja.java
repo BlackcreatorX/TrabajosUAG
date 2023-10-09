c3d96d34323b5a5baa478d415ef3d6f86080f893
Carlos Gustavo Valladares Corral <carlosvalladares2109@gmail.com>
2023-10-09 13:12:36 -0600

    practica de ventana

--- POO/granja/Granja.java
+++ POO/granja/Granja.java
@@ -1,97 +1,98 @@
-package clase_1;
+package granja;
+
 import java.util.Scanner;
 
 public class Granja {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	String nombre;
-	String direccion;
-	String numero;
-	double num1,num2;
-	
-	//VARIBALES
-	
-/*	
-	//ADQUIRIR DATOS E IMPRIMIR
-	Scanner scanf = new Scanner(System.in);
-	
-	System.out.println("dame el nombre de la granja");
-	
-	nombre=scanf.nextLine();
-	
-	System.out.println("El nombre de la granja es: "+nombre);
-	
-	
-	
-	Scanner scandir = new Scanner(System.in);
-	
-	System.out.println("dame la direccion de la granja");
-	
-	direccion=scandir.nextLine();
-	
-	System.out.println("La direccion de la granja es: "+direccion);
-	
-	
-	
-	Scanner scannum = new Scanner(System.in);
-	
-	System.out.println("dame el numero de la grnaja");
-	
-	numero=scannum.nextLine();
-	
-	System.out.println("El numero de la granja es: "+numero);*/
-	
-	System.out.println("danos el numero 1");
+		String nombre;
+		String direccion;
+		String numero;
+		double num1, num2;
 
-	Scanner scannum = new Scanner(System.in);
-	num1=scannum.nextDouble();
-	System.out.println("danos el numero 2");
+		// VARIBALES
 
-	Scanner scannum2 = new Scanner(System.in);
-	num2=scannum2.nextDouble();
-	
-	
-	suma(num1, num2);
-	res(num1, num2);
-	div(num1, num2);
-	mul(num1, num2);
-	
-	
+		/*
+		 * //ADQUIRIR DATOS E IMPRIMIR
+		 * Scanner scanf = new Scanner(System.in);
+		 * 
+		 * System.out.println("dame el nombre de la granja");
+		 * 
+		 * nombre=scanf.nextLine();
+		 * 
+		 * System.out.println("El nombre de la granja es: "+nombre);
+		 * 
+		 * 
+		 * 
+		 * Scanner scandir = new Scanner(System.in);
+		 * 
+		 * System.out.println("dame la direccion de la granja");
+		 * 
+		 * direccion=scandir.nextLine();
+		 * 
+		 * System.out.println("La direccion de la granja es: "+direccion);
+		 * 
+		 * 
+		 * 
+		 * Scanner scannum = new Scanner(System.in);
+		 * 
+		 * System.out.println("dame el numero de la grnaja");
+		 * 
+		 * numero=scannum.nextLine();
+		 * 
+		 * System.out.println("El numero de la granja es: "+numero);
+		 */
+
+		System.out.println("danos el numero 1");
+
+		Scanner scannum = new Scanner(System.in);
+		num1 = scannum.nextDouble();
+		System.out.println("danos el numero 2");
+
+		Scanner scannum2 = new Scanner(System.in);
+		num2 = scannum2.nextDouble();
+
+		suma(num1, num2);
+		res(num1, num2);
+		div(num1, num2);
+		mul(num1, num2);
+
 	}
-	public static double suma (double num1,double num2) {
-		
-		return num1+num2;
-				
+
+	public static double suma(double num1, double num2) {
+
+		return num1 + num2;
+
 	};
-	
-	public static double res (double num1,double num2) {
-		
-		return num1-num2;
-				
+
+	public static double res(double num1, double num2) {
+
+		return num1 - num2;
+
 	};
-	public static double div (double num1,double num2) {
-		
-		return num1/num2;
-				
+
+	public static double div(double num1, double num2) {
+
+		return num1 / num2;
+
 	};
-	public static double mul (double num1,double num2) {
-		
-		return num1*num2;
-				
+
+	public static double mul(double num1, double num2) {
+
+		return num1 * num2;
+
 	};
-	
-	
 
 }
 
-
-
-/*codig viejo
- Scanner scanner = new Scanner (System.in);
- 
-scanner.nextLine();
-
-
-Scanner pepe = new Scanner (System.in);
-pepe.nextLine()*/
+/*
+ * codig viejo
+ * Scanner scanner = new Scanner (System.in);
+ * 
+ * scanner.nextLine();
+ * 
+ * 
+ * Scanner pepe = new Scanner (System.in);
+ * pepe.nextLine()
+ */
--- POO/ventanas/PracticaVentana.java
+++ POO/ventanas/PracticaVentana.java
@@ -1,5 +1,45 @@
+
+//crear una ventana que diga hello world
+//importar librerias
 package ventanas;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 public class PracticaVentana {
 
+    // crear un metodo main
+    public static void main(String[] args) {
+        // llamar al metodo crearVentana
+        crearVentana();
+
+    }
+
+    // crear un metodo que cree una ventana
+    public static void crearVentana() {
+        // crear un objeto de tipo JFrame
+        JFrame ventana = new JFrame("Mi primera ventana");
+        // crear un objeto de tipo JPanel
+        JPanel panel = new JPanel();
+        // crear un objeto de tipo JLabel
+        JLabel etiqueta = new JLabel("Hello World");
+        // crear un objeto de tipo JTextField
+        JTextField texto = new JTextField(20);
+        // agregar el texto al JTextField
+        texto.setText("Hola Mundo");
+        // agregar el texto al JLabel
+        etiqueta.setText(texto.getText());
+        // agregar el JLabel al JPanel
+        panel.add(etiqueta);
+        // agregar el JPanel al JFrame
+        ventana.add(panel);
+        // hacer visible la ventana
+        ventana.setVisible(true);
+        // establecer el tamaño de la ventana
+        ventana.setSize(400, 400);
+        // establecer la operacion por defecto al cerrar la ventana
+        ventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
